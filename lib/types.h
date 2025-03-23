380428fd13fd307d64e4c59589c4a99a2156e40c
Moddimation <vakhidmytr06@gmail.com>
2025-03-23 18:14:26 +0100

    Attempt to use py script, letting this stay for now

--- lib/types.h
+++ lib/types.h
@@ -49,20 +49,7 @@ typedef u64 unk64;
 
 #ifndef __cplusplus
 typedef u16 wchar_t;
-#endif
-
-/* uint<x>_t */
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-/* int<x>_t */
-typedef char int8_t;
-typedef short int16_t;
-typedef int int32_t;
-
-typedef int intptr_t;
-typedef unsigned int uintptr_t;
-
 typedef unsigned long size_t;
+#endif
 
 #endif // _TYPES_H
